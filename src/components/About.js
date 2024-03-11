cb73bc71f86d9b2597084aab4cd5ed66edbc3088
Arjun <arjunnaik0402@gmail.com>
2024-03-11 23:59:13 +0530

    FE

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import noteContext from '../context/notes/noteContext'
+
+const About =() => {
+    const a = useContext(noteContext)
+  return (
+    <>
+        <div>
+            this is about {a.name}
+        </div>
+    </>
+  )
+}
+
+export default About
