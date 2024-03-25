f8aa0cf339b875cd6cceb3ced200a83d590b9c06
Arjun <arjunnaik0402@gmail.com>
2024-03-25 12:23:09 +0530

    About

--- src/components/About.js
+++ src/components/About.js
@@ -5,25 +5,25 @@ const About =() => {
   return (
     <>
     <div className='container my-2'>
-    <h1>About Us</h1>
-    <h4>Welcome to iNoteBook! We are dedicated to providing a secure and efficient platform for managing your notes online.</h4>
+    <h2>About Us</h2>
+    <h5>Welcome to iNoteBook! We are dedicated to providing a secure and efficient platform for managing your notes online.</h5>
     </div>
     <div className='container my-5'>
     <h2>Our Mission</h2>
-    <h4>At iNoteBook, our mission is to empower users with a reliable and secure tool to organize their thoughts, ideas, and tasks. We understand the importance of privacy and security when it comes to personal and sensitive information, which is why we prioritize the protection of your data.</h4>
+    <h5>At iNoteBook, our mission is to empower users with a reliable and secure tool to organize their thoughts, ideas, and tasks. We understand the importance of privacy and security when it comes to personal and sensitive information, which is why we prioritize the protection of your data.</h5>
     </div>
     <div className="container my-5">
     <h2>What We Offer</h2>
       <ul>
-        <li><h4>Secure Note Management: Easily create, update, and delete notes with confidence, knowing that your data is encrypted and protected.</h4></li>
-        <li><h4>User-Friendly Interface: Our intuitive interface makes it simple to organize your notes and access them from any device.</h4></li>
-        <li><h4>Cloud Storage: Your notes are securely stored in the cloud, ensuring accessibility and reliability.</h4></li>
-        <li><h4>Customization: Customize your notes with various formatting options to suit your preferences and needs.</h4></li>
+        <li><h5>Secure Note Management: Easily create, update, and delete notes with confidence, knowing that your data is encrypted and protected.</h5></li>
+        <li><h5>User-Friendly Interface: Our intuitive interface makes it simple to organize your notes and access them from any device.</h5></li>
+        <li><h5>Cloud Storage: Your notes are securely stored in the cloud, ensuring accessibility and reliability.</h5></li>
+        <li><h5>Customization: Customize your notes with various formatting options to suit your preferences and needs.</h5></li>
       </ul>
       </div>
       <div className='container my-5'>
     <h2>Contact Us</h2>
-    <h4>We value feedback from our users and are always here to assist you. If you have any questions, suggestions, or concerns, please don't hesitate to reach out to us at arjunnaik0402@gmail.com. Your satisfaction is our top priority, and we're here to help in any way we can.</h4>
+    <h5>We value feedback from our users and are always here to assist you. If you have any questions, suggestions, or concerns, please don't hesitate to reach out to us at arjunnaik0402@gmail.com. Your satisfaction is our top priority, and we're here to help in any way we can.</h5>
     </div>
     
     </>
